36145b20103d1f6bcf2a933000992e8c3ef99104
Robson Duarte <robson.o.d@gmail.com>
2016-02-12 10:02:57 -0200

    lecture 11

---
+++ learn-angularjs/src/main/webapp/lecture-11/app.js
@@ -0,0 +1,16 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("mainController", function($scope) {
+
+	$scope.name = 'Robson';
+	$scope.occupation = 'Developer';
+
+
+	$scope.getName = function() {
+		return 'Robson Duarte'
+	}
+
+	console.log($scope);
+});
+
+
