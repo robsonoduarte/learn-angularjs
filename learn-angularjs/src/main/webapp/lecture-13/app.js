9aab21207de3ab3a49e28bd4847e84fedc299f1a
Robson Duarte <robson.o.d@gmail.com>
2016-02-13 11:41:48 -0200

    lecture 13

---
+++ learn-angularjs/src/main/webapp/lecture-13/app.js
@@ -0,0 +1,12 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("mainController", function($scope) {
+	console.log($scope);
+});
+
+
+var searchPeople = function(firstName, $scope, height, age, occupation){
+	return 'Robson Duarte'
+}
+
+console.log(angular.injector().annotate(searchPeople));
