154e6b2fe6859cb871b6ba2fa2be746af9c4a66c
Robson Duarte <robson.o.d@gmail.com>
2016-02-16 10:13:18 -0200

    lecture 14

---
+++ learn-angularjs/src/main/webapp/lecture-14/app.js
@@ -0,0 +1,24 @@
+var myApp = angular.module("myApp", ['ngMessages','ngResource']);
+
+myApp.controller("mainController", function($scope,$log,$filter,$resource) {
+
+	$log.log("Hello.");
+	$log.info("this is some information.");
+	$log.warn("warning.");
+	$log.debug("Some debug infomartion while writing my code.");
+	$log.error("This was an error");
+
+
+
+	$scope.name = 'Robson Duarte';
+	$scope.formattedName = $filter('uppercase')($scope.name);
+
+
+
+	$log.info($scope.name);
+	$log.info($scope.formattedName);
+
+
+	$log.info($resource);
+
+});
