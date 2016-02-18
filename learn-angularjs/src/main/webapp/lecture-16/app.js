ed7e55a836502b76ece154056278ae436f78450c
Robson Duarte <robson.o.d@gmail.com>
2016-02-18 15:27:48 -0200

    lecture 16 starting.

---
+++ learn-angularjs/src/main/webapp/lecture-16/app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", function($scope,$log) {
+	$log.debug($scope);
+});
+
+
+
+// the the js minify broken the dependency injection of AngularJS
+//var myApp=angular.module("myApp",[]);myApp.controller("mainController",function(n,o){o.info(n)});
