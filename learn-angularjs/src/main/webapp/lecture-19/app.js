c72af8bac85af4bc559a8800ca2e01fd654ab92e
Robson Duarte <robson.o.d@gmail.com>
2016-02-20 12:25:44 -0200

    starting lecture 19.

---
+++ learn-angularjs/src/main/webapp/lecture-19/app.js
@@ -0,0 +1,15 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$filter',function($scope,$filter) {
+
+	$scope.handle = '';
+
+	$scope.loweCaseHandle = function () {
+		return $filter('lowercase')($scope.handle);
+	}
+
+
+}]);
+
+
