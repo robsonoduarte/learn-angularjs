079053f2a9f26476868e45a653e40d18e9d0f442
Robson Duarte <robson.o.d@gmail.com>
2016-02-20 11:25:26 -0200

    finished lecture 17.

---
+++ learn-angularjs/src/main/webapp/lecture-17/app.js
@@ -0,0 +1,15 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$timeout',function($scope,$timeout) {
+
+
+	$scope.name = 'Robson Duarte';
+
+	$timeout(function name() {
+		$scope.name = "EveryBody";
+	}, 3000)
+
+}]);
+
+
