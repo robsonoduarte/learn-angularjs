77c31db94eab128d18396ec05d6a503fc365a5c5
Robson Duarte <robson.o.d@gmail.com>
2016-02-23 10:19:27 -0300

    lecture 20.

---
+++ learn-angularjs/src/main/webapp/lecture-20/app.js
@@ -0,0 +1,43 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$filter', '$timeout',function($scope,$filter,$timeout) {
+
+	$scope.handle = '';
+
+	$scope.loweCaseHandle = function () {
+		return $filter('lowercase')($scope.handle);
+	}
+
+
+
+
+	$scope.$watch('handle', function(newValue, oldValue) {
+		console.log("old: " + oldValue);
+		console.log("new: " + newValue);
+	})
+
+
+
+
+	// with js timeout function and $apply angular js
+	setTimeout(function() {
+		$scope.$apply(function() {
+			$scope.handle = 'jstimeout';
+			console.log('Scope change by js timeout! ');
+		})
+	}, 3000)
+
+
+
+
+	$timeout(function() {
+		$scope.handle = 'timeoutservice';
+		console.log('Scope change by timeout angular js service! ');
+	}, 4000)
+
+
+
+}]);
+
+
