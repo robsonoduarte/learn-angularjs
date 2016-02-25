7d2cd338e9344e3f3b8d2fa22f5ce404131045ac
Robson Duarte <robson.o.d@gmail.com>
2016-02-25 09:58:24 -0300

    lecture 22.

---
+++ learn-angularjs/src/main/webapp/lecture-22/app.js
@@ -0,0 +1,14 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$filter',function($scope,$filter) {
+
+	$scope.alerClick = function() {
+		alert('clicked')
+	}
+
+
+	$scope.name = 'Robson Duarte'
+}]);
+
+
