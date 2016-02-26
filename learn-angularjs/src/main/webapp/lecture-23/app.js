ae8d5044d1f27355d71214aff788a0ceba841422
Robson Duarte <robson.o.d@gmail.com>
2016-02-26 09:36:25 -0300

    start lecture 23.

---
+++ learn-angularjs/src/main/webapp/lecture-23/app.js
@@ -0,0 +1,27 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$filter',function($scope,$filter) {
+
+	$scope.handle = '';
+
+	$scope.loweCaseHandle = function () {
+		return $filter('lowercase')($scope.handle);
+	}
+
+	$scope.characters = 5;
+
+
+
+
+	$scope.rules = [
+	  {ruleName: 'Must be 5 characters'},
+	  {ruleName: 'Must not be used elsewhere'},
+	  {ruleName: 'Must be coll'},
+	];
+
+	console.log($scope.rules)
+
+}]);
+
+
