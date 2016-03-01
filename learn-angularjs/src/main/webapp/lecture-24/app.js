b75f37222e1e3676a17a3fd8a0f3bde9aa41a5e4
Robson Duarte <robson.o.d@gmail.com>
2016-03-01 10:28:06 -0300

    starting the Lecture 24

---
+++ learn-angularjs/src/main/webapp/lecture-24/app.js
@@ -0,0 +1,39 @@
+var myApp = angular.module("myApp", []);
+
+
+myApp.controller("mainController", ['$scope', '$filter', '$http',function($scope,$filter,$http) {
+
+	$scope.handle = '';
+
+	$scope.loweCaseHandle = function () {
+		return $filter('lowercase')($scope.handle);
+	}
+
+	$scope.characters = 5;
+
+
+	$http.get('/learn-angularjs/api/lecture23')
+		 .success(function(result) {
+			 $scope.rules = result;
+		 })
+		 .error(function(data, status) {
+		 	console.log(data)
+		 })
+
+
+
+	$scope.newRule = '';
+	$scope.addRule = function() {
+
+		$http.post('/learn-angularjs/api/lecture23', {newRule: $scope.newRule})
+		.success(function(result) {
+			$scope.rules = result;
+			$scope.newRule = '';
+		})
+		.error(function(data, status) {
+			console.log(data);
+		})
+	}
+}]);
+
+
