95540f79cd680fe79b6293371a55f2ed1115b49e
Robson Duarte <robson.o.d@gmail.com>
2016-03-10 09:16:02 -0300

    starting lecture 30.

---
+++ learn-angularjs/src/main/webapp/lecture-30/app.js
@@ -0,0 +1,46 @@
+var myApp = angular.module("myApp", ['ngRoute']);
+
+
+
+myApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/main.html',
+			controller: 'mainController'
+		})
+		.when('/second', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+		.when('/second/:num', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+})
+
+
+
+
+myApp.controller("mainController", ['$scope', '$location', '$log',function($scope,$location,$log) {
+	$scope.name = 'Main'
+
+	$log.main = 'Property from main';
+	$log.log($log);
+	$log.log($scope);
+
+}]);
+
+
+
+myApp.controller("secondController", ['$scope', '$location', '$log', '$routeParams',function($scope,$location, $log, $routeParams){
+	$scope.num = $routeParams.num || 1
+
+	$log.second = 'Property from second';
+	$log.log($log);
+	$log.log($scope);
+
+}]);
+
+
+
+
