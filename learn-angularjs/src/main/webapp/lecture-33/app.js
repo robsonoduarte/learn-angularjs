f65e2fd877abe1e642b49219e428d43c9056e56b
Robson Duarte <robson.o.d@gmail.com>
2017-01-11 09:09:40 -0200

    go back to the curse and start the lecture 33

---
+++ learn-angularjs/src/main/webapp/lecture-33/app.js
@@ -0,0 +1,53 @@
+var myApp = angular.module("myApp", ['ngRoute']);
+
+
+
+myApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/main.html',
+			controller: 'mainController'
+		})
+		.when('/second', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+		.when('/second/:num', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+})
+
+
+
+
+
+myApp.service('nameService', function() {
+	var self = this
+
+	this.name = 'Robson Duarte'
+
+	this.nameLength = function() {
+		return self.name.length
+	}
+})
+
+
+
+
+
+myApp.controller("mainController", ['$scope', '$location', '$log', 'nameService', function($scope,$location,$log, nameService) {
+
+}]);
+
+
+
+
+
+myApp.controller("secondController", ['$scope', '$location', '$log', '$routeParams', 'nameService', function($scope,$location, $log, $routeParams,nameService){
+
+}]);
+
+
+
+
