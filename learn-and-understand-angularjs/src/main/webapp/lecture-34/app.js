062af02d06e4b6ae0eda1c2198e05243d47833ae
Robson Duarte <robson.o.d@gmail.com>
2017-12-08 15:43:12 -0200

    Lecuture 34.

---
+++ learn-and-understand-angularjs/src/main/webapp/lecture-34/app.js
@@ -0,0 +1,43 @@
+var myApp = angular.module("myApp", ['ngRoute']);
+
+
+
+myApp.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/main.html',
+			controller: 'mainController'
+		})
+		.when('/second', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+		.when('/second/:num', {
+			templateUrl: 'pages/second.html',
+			controller: 'secondController'
+		})
+})
+
+
+myApp.controller("mainController", ['$scope', '$location', '$log', function($scope,$location,$log) {
+
+}]);
+
+
+
+myApp.controller("secondController", ['$scope', '$location', '$log', '$routeParams', function($scope,$location, $log, $routeParams){
+
+}]);
+
+
+myApp.directive("searchResult", function() {
+	return {
+		restrict: 'AECM',
+		templateUrl: 'directives/searchresult.html',
+		replace: true
+	}
+});
+
+
+
+
