8d1893ed1b3a7fe94c263b83f7d3fa017b36d1ca
Robson Duarte <robson.o.d@gmail.com>
2017-12-14 12:27:05 -0200

    Lecture 46

---
+++ learn-and-understand-angularjs/src/main/webapp/lecture-46/app.js
@@ -0,0 +1,59 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+
+// ROUTES
+weatherApp.config(function($routeProvider) {
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'homeController'
+		})
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		})
+		.when('/forecast/:days', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		})
+})
+
+
+// SERVICES
+weatherApp.service('cityService', function() {
+	this.city = "London"
+})
+
+
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope','cityService', function($scope,cityService) {
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	})
+
+}]);
+
+
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope,$resource,$routeParams,cityService) {
+	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || '2';
+
+	$scope.weatherAPI =
+		$resource("http://api.openweathermap.org/data/2.5/forecast/daily?");
+
+	$scope.weatherResult =
+		$scope.weatherAPI.get({q:$scope.city, cnt:$scope.days, units:"metric", appid:"863979fd70e0b430fa19554cc22118fc"})
+
+
+	$scope.convertToDate = function(dt){
+		return new Date(dt * 1000);
+	}
+
+
+}]);
