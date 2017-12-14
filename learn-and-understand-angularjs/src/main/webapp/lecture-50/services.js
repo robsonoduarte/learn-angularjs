9fce16c1523392dd6885df98debe067b8db22e33
Robson Duarte <robson.o.d@gmail.com>
2017-12-14 16:23:12 -0200

    Lecture 50

---
+++ learn-and-understand-angularjs/src/main/webapp/lecture-50/services.js
@@ -0,0 +1,17 @@
+// SERVICES
+weatherApp.service('cityService', function() {
+	this.city = "London"
+});
+
+weatherApp.service('weatherService', function() {
+
+
+	this.GetWeather = function(city,day) {
+		var weatherAPI =
+			$resource("http://api.openweathermap.org/data/2.5/forecast/daily?");
+
+		return weatherAPI.get({q:city, cnt:days, units:"metric", appid:"863979fd70e0b430fa19554cc22118fc"})
+	}
+
+});
+
