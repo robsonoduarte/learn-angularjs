7f9c85dc89ec4a89f5a76d73f0dd9cac9b5a638b
Robson Duarte <robson.o.d@gmail.com>
2017-12-14 14:24:13 -0200

    Lecture 49.

---
+++ learn-and-understand-angularjs/src/main/webapp/lecture-49/controllers.js
@@ -0,0 +1,34 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope','$location', 'cityService', function($scope,$location,cityService) {
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	})
+
+
+	$scope.submit = function() {
+		$location.path("/forecast");
+	}
+
+}]);
+
+
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope,$resource,$routeParams,cityService) {
+	$scope.city = cityService.city;
+	$scope.days = $routeParams.days || '2';
+
+	$scope.weatherAPI =
+		$resource("http://api.openweathermap.org/data/2.5/forecast/daily?");
+
+	$scope.weatherResult =
+		$scope.weatherAPI.get({q:$scope.city, cnt:$scope.days, units:"metric", appid:"863979fd70e0b430fa19554cc22118fc"})
+
+
+	$scope.convertToDate = function(dt){
+		return new Date(dt * 1000);
+	}
+
+
+}]);
