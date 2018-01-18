1ad8792c3643009669ac4603ca5ea9bdbf395cd8
Robson Duarte <robson.o.d@gmail.com>
2018-01-18 11:00:13 -0200

    adicionando exemplos sobre variavies

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/typescript/js/variaveis.js
@@ -0,0 +1,10 @@
+// tipo string
+var message = "Help me, Obin-Wan Kenobi. You're my only hope!";
+console.log(message);
+// tipo number
+var epsisode = 4;
+console.log("This is episode " + epsisode);
+// tipo any
+var favoriteDroid;
+favoriteDroid = "BB-8";
+console.log("My favorite droid is " + favoriteDroid);
---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/typescript/variaveis.ts
@@ -0,0 +1,13 @@
+
+// tipo string
+let message: string = "Help me, Obin-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+// tipo number
+let epsisode: number = 4
+console.log("This is episode " + epsisode)
+
+// tipo any
+let favoriteDroid
+favoriteDroid = "BB-8"
+console.log("My favorite droid is " + favoriteDroid)
