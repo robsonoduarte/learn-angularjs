d39f9288d05d38233b5b4e037184927407bd1eaf
Robson Duarte <robson.o.d@gmail.com>
2018-01-19 10:21:58 -0200

    exemplo sobre funcoes en typescript

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/typescript/funcoes.ts
@@ -0,0 +1,19 @@
+
+// sintaxe padrao de uma funcção
+let isEnoughToBeatMF = function(parsecs: number): boolean{
+  return parsecs < 12
+}
+let distance = 14
+console.log(`Is ${distance} parsecs enough to beat Millennium Falcon ? ${isEnoughToBeatMF(distance) ? 'YES': 'NO'}`)
+
+// sintaxe arrow function
+let call = ( name: string) => console.log(`Do you copy, ${name}`)
+call('R2')
+
+// parametros padroes
+function inc(speed: number, inc: number = 1) : number {
+  return speed + inc
+}
+
+console.log(`inc(5,1) = ${inc(5,1)}`)
+console.log(`inc(5) = ${inc(5)}`)
---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/typescript/js/funcoes.js
@@ -0,0 +1,16 @@
+// sintaxe padrao de uma funcção
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon ? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+// sintaxe arrow function
+var call = function (name) { return console.log("Do you copy, " + name); };
+call('R2');
+// parametros padroes
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc(5,1) = " + inc(5, 1));
+console.log("inc(5) = " + inc(5));
