9f4463c2d9a8c7b88e8b66a7ba8a4f4ad6829b8e
Robson Duarte <robson.o.d@gmail.com>
2018-01-22 16:54:16 -0200

    adicionando exemplo com herança

--- construindo-aplicacoes-web-com-o-novo-angular-4/typescript/js/classe-e-interfaces.js
+++ construindo-aplicacoes-web-com-o-novo-angular-4/typescript/js/classe-e-interfaces.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // declarando um classe
 var Spacecraft = /** @class */ (function () {
     // usando a sintaxe reduzida para criar um atributo para a classe
@@ -13,3 +23,21 @@ var Spacecraft = /** @class */ (function () {
 //instânciando um objeto da classe
 var ship = new Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
+// usando herança
+var MillenniumFalcon = /** @class */ (function (_super) {
+    __extends(MillenniumFalcon, _super);
+    function MillenniumFalcon() {
+        return _super.call(this, 'hyperdrive') || this;
+    }
+    MillenniumFalcon.prototype.jumpIntoHyperspace = function () {
+        if (Math.random() >= 0.5) {
+            _super.prototype.jumpIntoHyperspace.call(this);
+        }
+        else {
+            console.log('Failed to jump into hyperspace');
+        }
+    };
+    return MillenniumFalcon;
+}(Spacecraft));
+var falcom = new MillenniumFalcon();
+falcom.jumpIntoHyperspace();
