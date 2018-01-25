ee38141c295538a79362ca9f528094acd0b0d059
Robson Duarte <robson.o.d@gmail.com>
2018-01-25 11:42:54 -0200

    exemplo de Property Binding

--- construindo-aplicacoes-web-com-o-novo-angular-4/jedi-academy/src/app/header/header.component.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/jedi-academy/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '=jad-header',
+  selector: 'jad-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/jedi-academy/src/app/student/student.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  name: string = 'Luke'
+  isJedi: boolean = true
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
