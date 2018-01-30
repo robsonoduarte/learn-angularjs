44022acf2f0f3a8bb0b3a83b0a822835d5caec08
Robson Duarte <robson.o.d@gmail.com>
2018-01-30 12:06:22 -0200

    adicionando tratamento de erro

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/app-error-handler.ts
@@ -0,0 +1,20 @@
+import {Response} from '@angular/http'
+import { Observable } from 'rxjs';
+
+export class ErrorHandler {
+  static handleError(error: Response | any){
+
+    let errorMessage: string
+
+    if(error instanceof Response){
+        errorMessage = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
+    }else{
+      errorMessage = error.toSring()
+    }
+
+    console.log(errorMessage)
+
+    return Observable.throw(errorMessage)
+
+  }
+}
