817bdf385a81ed288af85da999833adafe4177ee
Robson Duarte <robson.o.d@gmail.com>
2018-01-31 17:14:10 -0200

    add nova rota

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/app.routes.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import {Routes} from '@angular/router'
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'restaurants', component: RestaurantsComponent}
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent}
 ]
