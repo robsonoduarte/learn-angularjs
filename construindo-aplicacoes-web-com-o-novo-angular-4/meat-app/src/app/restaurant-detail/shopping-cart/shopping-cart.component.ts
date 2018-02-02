2b8fbb3ef1041821e3549b6ab4c7d82687c4de8c
Robson Duarte <robson.o.d@gmail.com>
2018-02-02 10:22:41 -0200

    integrando com o serviço

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,3 +1,5 @@
+import { CartItem } from './cart-item.model';
+import { ShoppingCartService } from './shopping-cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
 
+  
+  itens(): CartItem[]{
+    return this.shoppingCartService.itens
+  }
+  
+  total(): number{
+    return this.shoppingCartService.total()
+  }
+  
 }
