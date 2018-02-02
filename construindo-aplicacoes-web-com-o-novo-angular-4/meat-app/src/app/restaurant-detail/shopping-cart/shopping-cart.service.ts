489cf0782c824f76029612b713d4160b015e0df5
Robson Duarte <robson.o.d@gmail.com>
2018-02-02 10:21:54 -0200

    adicionando service para o cart item

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -0,0 +1,43 @@
+import { MenuItem } from '../menu-item/menu-item.model';
+import { CartItem } from './cart-item.model';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ShoppingCartService {
+
+  
+  itens: CartItem[] = []
+  
+  
+  constructor() { }
+
+  
+  clear(){
+    this.itens = []
+  }
+  
+  
+  addItem(item:MenuItem){
+    let foundItem = this.itens.find(mItem => mItem.nenuItem.id === item.id)
+    if(foundItem){
+      foundItem.quantity = foundItem.quantity + 1
+    }else{
+      this.itens.push(new CartItem(item))
+    }
+  }
+  
+  
+  removeItem(item:CartItem){
+    this.itens.splice(this.itens.indexOf(item), 1)
+  }
+  
+  
+  total(): number{
+    return this.itens
+      .map(item => item.value())
+      .reduce((prev,value) => prev + value, 0)
+  }
+  
+ 
+ 
+}
