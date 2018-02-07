94b283faabb7174a59fd21eff0f4225ae18b032a
Robson Duarte <robson.o.d@gmail.com>
2018-02-07 10:53:02 -0200

    usando o componente radio.

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/order/order.component.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/order/order.component.ts
@@ -1,3 +1,4 @@
+import { RadioOption } from '../shared/radio/radio-option.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  
+  
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value : 'MON'},
+    {label: 'Cartão de Débito', value : 'DEB'},
+    {label: 'Cartão Refeição', value : 'REF'}
+  ]
+  
   constructor() { }
 
   ngOnInit() {
