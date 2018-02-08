a21a12c759820f47b47de00103cc2dd84d657f66
Robson Duarte <robson.o.d@gmail.com>
2018-02-08 12:44:29 -0200

    novo componente de order item

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/order/order-itens/order-itens.component.ts
@@ -0,0 +1,37 @@
+import { CartItem } from '../../restaurant-detail/shopping-cart/cart-item.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'mt-order-itens',
+  templateUrl: './order-itens.component.html'
+})
+export class OrderItensComponent implements OnInit {
+
+  
+  @Input() itens: CartItem[]
+  
+  @Output() increseQty = new EventEmitter<CartItem>()
+  @Output() decreseQty = new EventEmitter<CartItem>()
+  @Output() remove = new EventEmitter<CartItem>()
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  
+  emitIncreaseQty(item: CartItem){
+    this.increseQty.emit(item)
+  }
+  
+  
+  emitDecreaseQty(item: CartItem){
+    this.decreseQty.emit(item)
+  }
+  
+  emitRemove(item: CartItem){
+    this.remove.emit(item)
+  }
+  
+  
+}
