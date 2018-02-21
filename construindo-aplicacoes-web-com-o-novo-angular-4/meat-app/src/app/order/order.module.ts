0915b33ddbd9fbcca2dde9ad535100dd771ed5a2
Robson Duarte <robson.o.d@gmail.com>
2018-02-21 09:57:38 -0300

    new module.

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/order/order.module.ts
@@ -0,0 +1,22 @@
+import { SharedModule } from '../shared/shared.module';
+import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
+import { OrderItensComponent } from './order-itens/order-itens.component';
+import { OrderComponent } from './order.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+const ROUTES: Routes = [
+ {path: '', component: OrderComponent},
+]
+
+
+@NgModule({
+    declarations:[OrderComponent,OrderItensComponent,DeliveryCostsComponent],
+    imports: [SharedModule, RouterModule.forChild(ROUTES)]
+})
+
+export class OrderModule {
+  
+}
