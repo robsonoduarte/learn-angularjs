a4dadc1615b292e8a032bea759b6fbb726677658
Robson Duarte <robson.o.d@gmail.com>
2018-02-21 09:37:23 -0300

    add shared module

---
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { InputComponent } from './input/input.component';
+import { RadioComponent } from './radio/radio.component';
+import { RatingComponent } from './rating/rating.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations:[InputComponent,RadioComponent,RatingComponent],
+  imports:[CommonModule,FormsModule,ReactiveFormsModule],
+  exports:[InputComponent,RadioComponent,RatingComponent,CommonModule,FormsModule,ReactiveFormsModule]
+})
+
+export class SharedModule {
+  
+}
