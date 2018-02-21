10da002bdec9d446d51c0ae25ecf099025192394
Robson Duarte <robson.o.d@gmail.com>
2018-02-21 09:59:02 -0300

    organize imports.

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/about/about.module.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/about/about.module.ts
@@ -12,7 +12,7 @@ const ROUTES: Routes = [
 
 @NgModule({
   declarations: [AboutComponent],
-  imports :[RouterModule, RouterModule.forChild(ROUTES)]
+  imports :[RouterModule.forChild(ROUTES)]
 })
 
 export class AboutModule {
