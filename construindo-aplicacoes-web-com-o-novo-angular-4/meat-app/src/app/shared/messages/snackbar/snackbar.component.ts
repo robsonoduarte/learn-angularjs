3f5af72379f247108604744811a04da2205ad9c8
Robson Duarte <robson.o.d@gmail.com>
2018-02-22 11:05:40 -0300

    add configuration to animation.

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/shared/messages/snackbar/snackbar.component.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,18 +1,39 @@
+import { animate } from '@angular/animations';
+import { transition } from '@angular/animations';
+import { style } from '@angular/animations';
+import { state } from '@angular/animations';
+import { trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity:0,
+        bottom: '0px'
+      })),
+      state('visible', style({
+        opacity:1,
+        bottom: '30px'        
+      })),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out')),
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
 
   
   message = 'Hello There !'
+  snackVisibility = 'hidden'
   
   constructor() { }
 
   ngOnInit() {
   }
 
+  
 }
