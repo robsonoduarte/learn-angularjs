a7892cc767d0b9b93b80006bf15e4a56b956d1aa
Robson Duarte <robson.o.d@gmail.com>
2018-02-23 10:36:22 -0300

    using the do and swicthMap operators.

--- construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/shared/messages/snackbar/snackbar.component.ts
+++ construindo-aplicacoes-web-com-o-novo-angular-4/meat-app/src/app/shared/messages/snackbar/snackbar.component.ts
@@ -7,6 +7,8 @@ import { trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'mt-snackbar',
@@ -36,12 +38,12 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message =>{
+    this.notificationService.notifier.do(message =>{
       this.message = message;
       this.snackVisibility = 'visible'
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden')
-    })
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibility = 'hidden')
   }
-
+  
   
 }
